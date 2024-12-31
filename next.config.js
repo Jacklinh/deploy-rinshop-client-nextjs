1bdfcd234032228ceb738be458ad77b10abfda86
Jacklinh <jacklinh79@gmail.com>
2024-12-31 20:43:49 +0700

    update api image

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['deploy-rinshop-restfulapi-mongoose.vercel.app']
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'deploy-rinshop-restfulapi-mongoose.vercel.app',
+        port: '',
+        pathname: '/**',
+      },
+    ],
   }
 }
 
